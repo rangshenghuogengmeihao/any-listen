fb01e351bf7494c53bf1063003b4f2f97130006b
lyswhut <lyswhut@qq.com>
2025-03-02 14:10:57 +0800

    feat: Optimize playback logic

--- packages/view-main/src/plugins/player/index.ts
+++ packages/view-main/src/plugins/player/index.ts
@@ -385,7 +385,8 @@ export const setPitchShifter = (val: number) => {
 export const hasInitedAdvancedAudioFeatures = (): boolean => audioContext != null
 
 export const setResource = (src: string) => {
-  if (audio) audio.src = src
+  if (!audio) return
+  audio.src = src
 }
 
 export const setPlay = () => {
